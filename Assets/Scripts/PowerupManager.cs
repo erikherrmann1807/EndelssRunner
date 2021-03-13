22d49fa99e63353983372efbfb3549c3a6cd9b0d
SinisterBlade-strv <75038401+SinisterBlade-strv@users.noreply.github.com>
2021-03-13 17:25:00 +0100

    All

---
+++ Assets/Scripts/PowerupManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+
+    private bool doublePoints;
+    private bool safeMode;
+
+    private bool powerupActive;
+
+    private float powerupLengthCounter;
+
+    private ScoreManager theScoreManager;
+    private PlatformGenerator thePLatformGenerator;
+
+    private float normalPointsPerSecond;
+    private float spikeRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theScoreManager = FindObjectOfType<ScoreManager>();
+        thePLatformGenerator = FindObjectOfType<PlatformGenerator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(powerupActive)
+        {
+            powerupLengthCounter -= Time.deltaTime;
+
+            if(doublePoints)
+            {
+                theScoreManager.pointsPerSecond = normalPointsPerSecond * 2;
+                theScoreManager.shouldDouble = true;
+            }
+
+            if(safeMode)
+            {
+                thePLatformGenerator.randomSpikeThreshold = 0;
+            }
+
+            if(powerupLengthCounter <= 0)
+            {
+                theScoreManager.pointsPerSecond = normalPointsPerSecond;
+                theScoreManager.shouldDouble = false;
+
+                thePLatformGenerator.randomSpikeThreshold = spikeRate;
+                powerupActive = false;
+            }
+        }
+    }
+
+    public void ActivatePowerup(bool points, bool safe, float time)
+    {
+        doublePoints = points;
+        safeMode = safe;
+        powerupLengthCounter = time;
+
+        normalPointsPerSecond = theScoreManager.pointsPerSecond;
+        spikeRate = thePLatformGenerator.randomSpikeThreshold;
+
+        powerupActive = true;
+    }
+}
