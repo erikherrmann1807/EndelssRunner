e92f2ac587cc181bd7f656eafa385fdac0e8c8f9
Sinister Blade-strv <erikherrmann1@gmx.de>
2021-06-05 10:51:01 +0200

    Add coin pick up and placeholder icons

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    private int _coinValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            CoinPickUp.instance.CoinCounter(_coinValue);
+        }
+    }
+}
---
+++ Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinPickUp : MonoBehaviour {
+    public static CoinPickUp instance;
+    public int scoreToGive;
+    private int coin;
+
+    public Text _coinText;
+    private ScoreManager _theScoreManager;
+
+    private AudioSource _coinSound;
+
+    private PowerUps _myPowerUps;
+
+    // Start is called before the first frame update
+    private void Start() {
+
+        if (instance == null) {
+            instance = this;
+        }
+        
+        _theScoreManager = FindObjectOfType<ScoreManager>();
+        _myPowerUps = FindObjectOfType<PowerUps>();
+        _coinText = GameObject.Find("CoinText").GetComponent<Text>();
+        
+        scoreToGive *= _myPowerUps.multiplier;
+
+        _coinSound = GameObject.Find("CoinSound").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        //_coinText.text = "Coins: " + coins;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "Player") {
+            _theScoreManager.AddScore(scoreToGive);
+            gameObject.SetActive(false);
+
+            if (_coinSound.isPlaying) {
+                _coinSound.Stop();
+                _coinSound.Play();
+            }
+            else {
+                _coinSound.Play();
+            }
+        }
+    }
+
+    public void CoinCounter(int coinValue) {
+        coin += coinValue;
+        _coinText.text = "Coins: " + coin;
+        Debug.Log("Hochgezählt!");
+    }
+}
